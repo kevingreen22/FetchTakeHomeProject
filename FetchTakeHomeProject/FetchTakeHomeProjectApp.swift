d00406595931875d627005f7f88fe77de2ad31e7
Kevin Green <46732563+kevingreen22@users.noreply.github.com>
2025-02-01 09:23:35 -0800

    Initial Commit

---
+++ FetchTakeHomeProject/FetchTakeHomeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchTakeHomeProjectApp.swift
+//  FetchTakeHomeProject
+//
+//  Created by Kevin Green on 2/1/25.
+//
+
+import SwiftUI
+
+@main
+struct FetchTakeHomeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
